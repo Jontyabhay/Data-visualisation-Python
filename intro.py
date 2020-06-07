c173d1f4153e3f7f97948481cb79626a0e5d4d49
Akshay Mishra <42304248+Jontyabhay@users.noreply.github.com>
2020-06-08 00:53:20 +0530

    Create intro.py

---
+++ intro.py
@@ -0,0 +1,16 @@
+lovely_loveseat_description = 'Lovely Loveseat. Tufted polyester blend on wood. 32 inches high x 40 inches wide x 30 inches deep. Red or white.'
+lovely_loveseat_price = 254.00
+stylish_settee_description = 'Stylish Settee. Faux leather on birch. 29.50 inches high x 54.75 inches wide x 28 inches deep. Black.'
+stylish_settee_price = 180.50
+luxurious_lamp_description = 'Luxurious Lamp. Glass and iron. 36 inches tall. Brown with cream shade.'
+luxurious_lamp_price = 52.15
+sales_tax = .088
+customer_one_total = 254.00
+customer_one_total += luxurious_lamp_price
+customer_one_tax = customer_one_total * sales_tax
+customer_one_total += customer_one_tax
+customer_one_itemization = "Lovely Loveseat. Tufted polyester blend on wood. 32 inches high x 40 inches wide x 30 inches deep. Red or white."
+print("Customer One Items:")
+print(customer_one_itemization)
+print("Customer One Total:")
+print(customer_one_total)
